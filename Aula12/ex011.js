a809e1f80838a2ea6c0a39a8cbbe12c1b86eb0d3
Cleiton <tirandoonda2014@gmail.com>
2022-02-22 15:16:14 -0300

    Adiionado exercicio 11

---
+++ Aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 25
+
+if (idade<16) {
+        console.log('Não pode votar')
+} else if (idade < 18) {
+        console.log('Voto facultativo')
+} else {
+        console.log('Voto obrigatorio')
+}
