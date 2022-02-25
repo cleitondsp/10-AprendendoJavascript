8f1c964bcf06ca9bbe04b3e67d4f96bbcdfaa197
Cleiton <tirandoonda2014@gmail.com>
2022-02-25 10:19:05 -0300

    Adicionado exercicio sobre vetores

--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -1,5 +1,26 @@
-let num = [6,8,2,8,5,3]
+let valores = [8,1,7,4,2,9]
 
-for (var count=0; count < num.length; count++) {
-    console.log(`A posição ${count} do vetor é ${num[count]}`)
+/* console.log(valores)
+
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5]) */
+
+/* for (var count=0; count < valores.length; count++) {
+    console.log(`A posição ${count} do vetor é ${valores[count]}`)
+} */
+
+/* simplificando o for */
+
+for ( let pos in valores) { /* Para cada posição (pos) na variavel (valores) faça ... */
+    console.log(`A posição ${pos} do vetor é ${valores[pos]}`)
 }
+
+
+/* Procurando valores... */
+
+console.log(valores.indexOf(4)) /* procurando o 4 */
+console.log(valores.indexOf(3))
