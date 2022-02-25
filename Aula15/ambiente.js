7de356803c57071903d6d0310973d59f17a8c0a7
Cleiton <tirandoonda2014@gmail.com>
2022-02-25 10:02:37 -0300

    Adicionado auça sobre vetor

---
+++ Aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [6,8,2,8,5,3]
+
+ordem = num.sort()
+
+console.log(ordem)
+
+num.push(4) /* adicionar ao vetor */
+
+console.log(ordem)
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let num = [6,8,2,8,5,3]
+
+for (var count=0; count < num.length; count++) {
+    console.log(`A posição ${count} do vetor é ${num[count]}`)
+}
